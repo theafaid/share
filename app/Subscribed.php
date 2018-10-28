58103767be4374cb5d3a05f25d070c320f49e1ca
Abdulrahman Faid <abdulrahmanfaid11@gmail.com>
2018-10-28 16:41:39 +0200

    User Can Subscribe To  a Thread

---
+++ app/Subscribed.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JanOrFeb
+ * Date: 10/28/2018
+ * Time: 4:41 PM
+ */
+
+namespace App;
+
+
+trait Subscribed
+{
+
+}
---
+++ resources/views/activities/created_subscription.blade.php
@@ -0,0 +1,27 @@
+@if($activity->subscribed_type == 'App\Thread')
+    @component('components.activity')
+        @slot('title')
+                {{$user->username}} has Subscribed a thread
+            <a href="{{route('threads.show', $activity->subject->subscribed->slug)}}">{{$activity->subject->subscribed->title}}</a>
+            <span class="btn btn-info float-right">{{$date}}</span>
+        @endslot
+
+        @slot('body')
+            {{str_limit($activity->subject->subscribed->body, 300)}}
+        @endslot
+    @endcomponent
+@else
+
+    @component('components.activity')
+        @slot('title')
+            {{$user->username}} has Liked a reply onsss
+            <a href="{{$activity->subject->likable->path()}}">{{$activity->subject->likable->thread->title}}</a>
+            <span class="btn btn-info float-right">{{$date}}</span>
+        @endslot
+
+        @slot('body')
+            {{str_limit($activity->subject->likable->body, 300)}}
+        @endslot
+    @endcomponent
+
+@endif
